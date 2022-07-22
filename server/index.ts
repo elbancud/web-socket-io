db1dea9875ab37368cb14ae9ca30b8308c8e67dc
emarson.halcyondigital <emarson.halcyondigital@gmail.com>
2022-07-22 11:00:40 +0800

    chore: initialize server

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+
+// initialize app
+const app = express();
+// initialize cors
+app.use(cors());
+
+// create server and http
+const server = http.createServer(app);
+// create io and connect the app
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
