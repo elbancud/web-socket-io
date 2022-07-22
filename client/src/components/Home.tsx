09543e06cce1a09dbd4d0c3b498c2719e6ee55e7
emarson.halcyondigital <emarson.halcyondigital@gmail.com>
2022-07-22 11:51:20 +0800

    chore: home page styled

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -29,29 +29,26 @@ export default function Example() {
       <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
         <div className='max-w-md w-full space-y-8'>
           <div>
-            <img
-              className='mx-auto h-12 w-auto'
-              src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
-              alt='Workflow'
-            />
+            <iframe
+              src='https://giphy.com/embed/wZ5kPbU0TXScE'
+              width='150'
+              height='150'
+              frameBorder='0'
+              className='giphy-embed m-auto'></iframe>
+
             <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
-              Sign in to your account
+              Welcome to the socket.
             </h2>
             <p className='mt-2 text-center text-sm text-gray-600'>
-              Or{' '}
-              <a
-                href='#'
-                className='font-medium text-indigo-600 hover:text-indigo-500'>
-                start your 14-day free trial
-              </a>
+              Let's see if shit works here.
             </p>
           </div>
-          <form className='mt-8 space-y-6' action='#' method='POST'>
+          <form className='mt-8 flex justify-center' action='#' method='POST'>
             <input type='hidden' name='remember' defaultValue='true' />
-            <div className='rounded-md shadow-sm -space-y-px'>
+            <div className='rounded-md shadow-sm -space-y-px w-100 inlin'>
               <div>
                 <label htmlFor='email-address' className='sr-only'>
-                  Email address
+                  Message
                 </label>
                 <input
                   id='email-address'
@@ -59,55 +56,25 @@ export default function Example() {
                   type='email'
                   autoComplete='email'
                   required
-                  className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                  placeholder='Email address'
+                  className='appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                  placeholder='Aa'
                 />
-              </div>
-              <div>
-                <label htmlFor='password' className='sr-only'>
-                  Password
-                </label>
-                <input
-                  id='password'
-                  name='password'
-                  type='password'
-                  autoComplete='current-password'
-                  required
-                  className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                  placeholder='Password'
-                />
-              </div>
-            </div>
-
-            <div className='flex items-center justify-between'>
-              <div className='flex items-center'>
-                <input
-                  id='remember-me'
-                  name='remember-me'
-                  type='checkbox'
-                  className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
-                />
-                <label
-                  htmlFor='remember-me'
-                  className='ml-2 block text-sm text-gray-900'>
-                  Remember me
-                </label>
-              </div>
-
-              <div className='text-sm'>
-                <a
-                  href='#'
-                  className='font-medium text-indigo-600 hover:text-indigo-500'>
-                  Forgot your password?
-                </a>
               </div>
             </div>
 
             <div>
               <button
                 type='submit'
-                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-                Sign in
+                className='group relative w-full flex justify-center py-2 px-2 border border-transparent text-sm font-medium rounded-md'>
+                <span className='rotate-90'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-5 w-5'
+                    viewBox='0 0 20 20'
+                    fill='red'>
+                    <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z' />
+                  </svg>
+                </span>
               </button>
             </div>
           </form>
